e9ee18db5370185986880fac83fae872eed8e09b
Migoz2 <tadiwamello@gmail.com>
2025-10-09 22:17:01 +0200

    Update settings.js for King35

--- settings.js
+++ settings.js
@@ -1,8 +1,6 @@
 module.exports = {
-  SESSION_ID: "", // Put Session Id Here Inside "*"
-  OWNER_NUMBER: "263719647303", // Put Owner Number Here 
-  PREFIX: ".", // Default Prefix You Can Change To .,*!?+-&%#@
-  CDN: "https://mrfrankk-cdn.hf.space" // Dont change this part
+  SESSION_ID: "SUBZERO~3f2b28a0c9a33c715ccf067b70b2905f1ccfe542",
+  OWNER_NUMBER: "+263789916361", 
+  PREFIX: ".",
+  CDN: "https://mrfrankk-cdn.hf.space"
 };
-
-// CREATED BY DARRELL MUCHERI ( MR FRANK OFC 🇿🇼 )
